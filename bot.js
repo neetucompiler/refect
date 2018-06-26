59383a7f16632c1f3f02f60f407244a54f3aa8d0
neetucompiler <neetucompiler@gmail.com>
2018-06-26 05:53:48 -0400

    addeed console log check

--- bot.js
+++ bot.js
@@ -110,6 +110,9 @@ controller.hears('hello-intent', 'direct_message', dialogflowMiddleware.hears, f
   bot,
   message
 ) {
+  console.log('inside hello intent')
+  console.log(JSON.stringify(bot))
+  console.log(JSON.stringify(message))
   bot.reply(message, 'Howdy!!!!');
   bot.reply(message, 'Please enter your email id Reflection for Creation');
 });
